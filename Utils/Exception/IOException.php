15d6b8564adf59f1353eef65ee7da06de92c0d32
Eugen <eugen@ganshorn.eu>
2017-12-20 17:21:29 +0100

    SHTS-14967 implement command logic and big part of pipe bundle

---
+++ Utils/Exception/IOException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Shopping\ShellCommandBundle\Utils\Exception;
+
+/**
+ * @author    Eugen Ganshorn <eugen.ganshorn@check24.de>
+ * @author    Silvester Denk <silvester.denk@check24.de>
+ * @copyright 2017 CHECK24 Vergleichsportal Shopping GmbH <http://preisvergleich.check24.de>
+ */
+class IOException extends \RuntimeException
+{
+}
