bd4ec3e4dc0626644272c17a06642ee5e9956d91
Eugen Ganshorn <eugen.ganshorn@check24.de>
2018-10-30 11:02:07 +0100

    ignore exit code -1

--- Utils/Pipe/Component/LinearPipeComponent.php
+++ Utils/Pipe/Component/LinearPipeComponent.php
@@ -119,6 +119,10 @@ class LinearPipeComponent implements PipeComponentInterface, LoggerAwareInterfac
             ->setExpectedExitcodes($this->getExpectedExitCodes())
             ->onError(
                 function (Process $process) {
+                    if ($process->getExitCode() === -1) {
+                        return;
+                    }
+
                     throw new ShellCommandRuntimeError(sprintf(
                         'CMD: %s, PID: %d, Exit-Code: %d, Error: %s',
                         $process->getCommand()->serialize(),
